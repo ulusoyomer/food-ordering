e4ea6f4439bc73bff099403b6c28ab6e8f899c2b
Ömer Ulusoy <ulusoy.omer09@gmail.com>
2023-02-14 15:30:19 +0300

    Added nprogress

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -6,6 +6,13 @@ import { SessionProvider } from 'next-auth/react';
 
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Router from 'next/router';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 export default function App({
 	Component,
